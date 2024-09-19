d15a7741124e15643eee6462f095bb2cc5a4f470
Reni Christian <renisonreni@gmail.com>
2024-09-19 22:16:58 +0530

    fixed landing page issue 4

--- src/components/my/landing-page.tsx
+++ src/components/my/landing-page.tsx
@@ -1,5 +1,5 @@
 'use client'
-// new commit message 2
+// new commit message here
 import { useState, useEffect } from 'react'
 // import Image from 'next/image'
 import { Search, Users, UserCheck, ShoppingCart } from 'lucide-react'
